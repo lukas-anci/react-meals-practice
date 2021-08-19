896f9bab2c1e5dbd47addcf31fd148f8d9fd1685
Lukas <lukas.ancikevicius@gmail.com>
2021-08-19 09:45:04 +0300

    available meals almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Layout/Header';
+import AvailableMeals from './components/Meals/AvailableMeals';
 import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MealsSummary />
+      <main>
+        <MealsSummary />
+        <AvailableMeals />
+      </main>
     </div>
   );
 }
