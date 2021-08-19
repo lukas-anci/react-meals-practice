17763f3e3490ba4a3be5bc816e0950e3d3bf3d8f
Lukas <lukas.ancikevicius@gmail.com>
2021-08-19 11:09:30 +0300

    use input

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,7 +1,12 @@
 import classes from './Input.module.css';
 
-const Input = () => {
-  return <div></div>;
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
 };
 
 export default Input;
