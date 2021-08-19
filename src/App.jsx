97bd0b1b9bd2ce39008aa15f0dd2fafeaadaa845
Lukas <lukas.ancikevicius@gmail.com>
2021-08-19 09:30:04 +0300

    Meals summary functional component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MealsSummary />
     </div>
   );
 }
