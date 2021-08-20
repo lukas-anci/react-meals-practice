69f38f0e8d164caeae4a84a47c224dcef02b5077
Lukas <lukas.ancikevicius@gmail.com>
2021-08-20 08:23:19 +0300

    showCartBtn state, open modal workds

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import { useState } from 'react';
+
+// naujoj sakoj
+// uzduotis panaudoti state kintamaji ir valdyti modalo isjungima ir ijungima su atitinkamais mygtukais ir paspaudimais
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Cart />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,12 +2,12 @@ import css from './Header.module.css';
 import mealImg from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
       <header className={css.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={css['main-image']}>
         <img src={mealImg} alt="table full of food" />
