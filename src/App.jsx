78602e5ce3d12bbdb4215d7deb5c95cc87229cd6
Lukas <lukas.ancikevicius@gmail.com>
2021-08-20 08:51:57 +0300

    use cartProvider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import { useState } from 'react';
+import CartProvider from './store/CartProvider';
 
 // naujoj sakoj
 // uzduotis panaudoti state kintamaji ir valdyti modalo isjungima ir ijungima su atitinkamais mygtukais ir paspaudimais
@@ -19,13 +20,13 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <CartProvider className="App">
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   );
 }
 
