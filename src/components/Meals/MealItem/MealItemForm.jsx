939aab17c53128dbab40646215ba9635c230100b
Lukas <lukas.ancikevicius@gmail.com>
2021-08-20 11:02:01 +0300

    use ctx in MealItemForm + mini validation

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,13 +1,20 @@
+import { useState, useContext } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
-import { useState } from 'react';
+import CartContext from './../../../store/cart-context';
 
 const MealItemForm = (props) => {
   const [formQty, setFormQty] = useState(1);
+  const cartCtx = useContext(CartContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // isitikinti kad ivesta reiksme yra tap 1 ir 5 ir siusti tik tada kai reiksme tinkama
+    if (formQty.trim().length === 0 || +formQty < 1 || +formQty > 5) return;
     console.log('ivest', formQty);
+
+    cartCtx.addItem({ id: 'c1', name: 'sushi', amount: 2, price: 12.99 });
   };
 
   const inputValueHandler = (e) => {
@@ -22,8 +29,8 @@ const MealItemForm = (props) => {
         input={{
           id: 'amount_' + props.id,
           type: 'number',
-          min: 1,
-          max: 5,
+          // min: 1,
+          // max: 5,
           step: 1,
         }}
       />
