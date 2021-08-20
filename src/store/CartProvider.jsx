983939b407f3690dd9cff0f852ce7373732bf844
Lukas <lukas.ancikevicius@gmail.com>
2021-08-20 08:58:24 +0300

    carProvider has cartContext values

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,7 +1,19 @@
 import CartContext from './cart-context';
 
 const CartProvider = (props) => {
-  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCartHandler = (item) => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
